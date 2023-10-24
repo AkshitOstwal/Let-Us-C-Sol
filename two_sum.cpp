549ff6213a4084ed026330c6baadcfdc2420a916
Akshit Ostwal <akshitostwal@gmail.com>
2023-10-24 17:53:32 +0530

    Create two_sum.cpp

---
+++ two_sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
